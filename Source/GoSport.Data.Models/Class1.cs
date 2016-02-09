b64ca357446eff92da8bfa4e2595dd19f6bb453f
antoanelenkov <antoanelenkov@gmail.com>
2016-02-09 21:58:20 +0200

    Inital commit for GoSport Application

---
+++ Source/GoSport.Data.Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoSport.Data.Models
+{
+    public class Class1
+    {
+    }
+}
